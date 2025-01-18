d3c78c8ca8b40482b444f96687450563ab981b74
Erick Hernandez <erickhernandez0492@gmail.com>
2025-01-18 23:25:03 +0100

    feat:Cambio de idioma global y manualmente

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basics-page',
   templateUrl: './basics-page.component.html',
-  styleUrl: './basics-page.component.css'
+  styleUrl: './basics-page.component.css',
 })
 export class BasicsPageComponent {
+  public nameLower: string = 'erick';
+  public nameUpper: string = 'ERICK';
+  public fullName: string = 'eRiCK hERNandEz';
 
+  public customDate: Date = new Date();
 }
