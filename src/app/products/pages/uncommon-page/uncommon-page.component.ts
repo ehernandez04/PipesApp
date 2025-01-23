83a2a9b3d4c0d093e526888c4a71ce197ebce8eb
Erick Hernandez <erickhernandez0492@gmail.com>
2025-01-23 10:30:40 +0100

    feat: add i18n - I18nSelectPipe - I18nPruralPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,8 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrl: './uncommon-page.component.css'
+  styleUrl: './uncommon-page.component.css',
 })
 export class UncommonPageComponent {
+  // i18n Select
+  public name: string = 'Erick';
+  public gender: 'male' | 'female' = 'male';
 
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeClient() {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
+
+  // i18nPrular
+  public clients: string[] = [
+    'Maria',
+    'Pedro',
+    'Fernando',
+    'Hernando',
+    'Eduardo',
+    'Melissa',
+    'Natalia',
+  ];
+
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 personas esperando.',
+    other: 'tenemos # clientes esperando.',
+  };
+
+  deleteClient() {
+    this.clients.shift();
+  }
 }
